5a3d96375181d2c3504095265ee2bceeede9f9fe
Khaqan Ashraf <khaqan_ashraf@yahoo.com>
2023-11-21 18:47:47 +0500

    MIT license

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='stealth25519',
-    version='1.0.4',
+    version='1.0.5',
     description='The Stealth25519 Python package provides functionality for the generation, verification, and signing of stealth addresses using the standard ed25519 algorithm with the ECDH protocol.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
     tests_require=[],
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
     entry_points={
         'console_scripts': [],
     },
