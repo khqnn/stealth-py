6a5a25b82ce333de41f6f69cafebbe1e97d6bf92
Khaqan Ashraf <khaqan_ashraf@yahoo.com>
2023-11-21 18:29:01 +0500

    library inline documentation

--- stealth25519/StealthAddress.py
+++ stealth25519/StealthAddress.py
@@ -5,23 +5,70 @@ import random
 p, q = get25519Params()
 
 class PublicKey:
+    """
+    Represents a public key for Ed25519-based cryptography.
+
+    Attributes:
+        publicBytes (bytes): The raw bytes of the public key.
+        B (Point): The point on the curve corresponding to the public key.
+
+    Methods:
+        getPublicBytes(): Get the raw bytes of the public key.
+        getPointOnCurve(): Get the point on the curve corresponding to the public key.
+        __str__(): Return a hexadecimal representation of the public key.
+    """
+
     def __init__(self, publicBytes = None):
+        """
+        Initialize a PublicKey instance.
+
+        Args:
+            publicBytes (bytes): The raw bytes of the public key.
+        """
+
         self.publicBytes = publicBytes
         self.B = point_decompress(publicBytes)
 
     def getPublicBytes(self):
+        """Get the raw bytes of the public key."""
+
         return self.publicBytes
 
     def getPointOnCurve(self):
+        """Get the point on the curve corresponding to the public key."""
+
         return self.B
 
     def __str__(self):
+        """Return a hexadecimal representation of the public key."""
+
         return self.publicBytes.hex()
 
 
 
 class PrivateKey:
+    """
+    Represents a private key for Ed25519-based cryptography.
+
+    Attributes:
+        privateBytes (bytes): The raw bytes of the private key.
+        b (int): The private scalar used in key generation.
+        publicKey (PublicKey): The corresponding public key.
+
+    Methods:
+        generatePublicKey(): Generate the corresponding public key.
+        getPrivateBytes(): Get the raw bytes of the private key.
+        __str__(): Return a hexadecimal representation of the private key.
+    """
+
     def __init__(self, privateBytes = None):
+        """
+        Initialize a PrivateKey instance.
+
+        Args:
+            privateBytes (bytes): The raw bytes of the private key.
+        """
+
         publicBytes = secret_to_public(privateBytes) # ed25519
         b, dummy = secret_expand(privateBytes) # ed25519
         
@@ -30,36 +77,86 @@ class PrivateKey:
         self.publicKey = PublicKey(publicBytes)
 
     def generatePublicKey(self):
+        """Generate the corresponding public key."""
+
         return self.publicKey
 
     def getPrivateBytes(self):
+        """Get the raw bytes of the private key."""
+
         return self.privateBytes
 
     def __str__(self):
+        """Return a hexadecimal representation of the private key."""
+
         return self.privateBytes.hex()
 
 
 
 class StealthAddress:
+    """
+    Represents a Stealth Address for Ed25519-based cryptography.
+
+    Attributes:
+        Rs (bytes): The raw bytes of the compressed R point.
+        Ps (bytes): The raw bytes of the compressed P point.
+        R (Point): The decompressed R point.
+
+    Methods:
+        __str__(): Return a formatted string representation of the Stealth Address.
+    """
+
     def __init__(self, R = None, P = None):
+        """
+        Initialize a StealthAddress instance.
+
+        Args:
+            R (bytes): The raw bytes of the compressed R point.
+            P (bytes): The raw bytes of the compressed P point.
+        """
+
         self.Rs = R
         self.Ps = P
         self.R = point_decompress(R) # ed25519
-        self.address = R + P
     
     def __str__(self):
+        """Return a formatted string representation of the Stealth Address."""
+
         return f'R: {self.Rs.hex()}\nP: {self.Ps.hex()}'
 
 
 
 class StealthAddressGenerator:
+    """
+    Generates Stealth Addresses for Ed25519-based cryptography.
+
+    Attributes:
+        publicSpendKey (PublicKey): The public spend key.
+        publicViewKey (PublicKey): The public view key.
+        V (Point): The decompressed point corresponding to the public view key.
+        B (Point): The decompressed point corresponding to the public spend key.
+
+    Methods:
+        generate(): Generate a Stealth Address.
+    """
+
     def __init__(self, publicSpendKey = None, publicViewKey = None):
+        """
+        Initialize a StealthAddressGenerator instance.
+
+        Args:
+            publicSpendKey (PublicKey): The public spend key.
+            publicViewKey (PublicKey): The public view key.
+        """
+
         self.publicSpendKey = publicSpendKey
         self.publicViewKey = publicViewKey
         self.V = point_decompress(publicViewKey.getPublicBytes()) # ed25519
         self.B = point_decompress(publicSpendKey.getPublicBytes()) # ed25519
         
     def generate(self):
+        """Generate a Stealth Address."""
+
         r = sha512_modq(random.randbytes(32))
         R = point_mul(r, G)
         Rs = point_compress(R)
@@ -74,7 +171,28 @@ class StealthAddressGenerator:
 
 
 class StealthAddressVerifier:
+    """
+    Verifies Stealth Addresses for Ed25519-based cryptography.
+
+    Attributes:
+        privateViewKey (PrivateKey): The private view key.
+        publicSpendKey (PublicKey): The public spend key.
+        vs (int): The expanded private view key scalar.
+        B (Point): The decompressed point corresponding to the public spend key.
+
+    Methods:
+        verify(stealthAddress): Verify the given Stealth Address.
+    """
+
     def __init__(self, privateViewKey, publicSpendKey):
+        """
+        Initialize a StealthAddressVerifier instance.
+
+        Args:
+            privateViewKey (PrivateKey): The private view key.
+            publicSpendKey (PublicKey): The public spend key.
+        """
+
         self.privateViewKey = privateViewKey
         self.publicSpendKey = publicSpendKey
         vs, dummy = secret_expand(privateViewKey.getPrivateBytes()) # ed25519
@@ -83,6 +201,12 @@ class StealthAddressVerifier:
 
 
     def verify(self, stealthAddress):
+        """Verify the given Stealth Address.
+
+        Args:
+            stealthAddress (StealthAddress): The Stealth Address to be verified.
+        """
+
         Rs, Ps = stealthAddress.Rs, stealthAddress.Ps
         f = point_compress(point_mul(self.vs, stealthAddress.R)) # ed25519
         h_ = sha512_modq(f) # ed25519
@@ -92,7 +216,31 @@ class StealthAddressVerifier:
 
 
 class StealthAddressSigner:
+    """
+    Signs Stealth Addresses for Ed25519-based cryptography.
+
+    Attributes:
+        privateSpendKey (PrivateKey): The private spend key.
+        privateViewKey (PrivateKey): The private view key.
+        bs (int): The expanded private spend key scalar.
+        vs (int): The expanded private view key scalar.
+
+    Methods:
+        sign(stealthAddress, msg): Sign the given Stealth Address and message.
+
+    Returns:
+        bool: True if the transaction is intended for the recipient
+    """
+
     def __init__(self, privateSpendKey, privateViewKey):
+        """
+        Initialize a StealthAddressSigner instance.
+
+        Args:
+            privateSpendKey (PrivateKey): The private spend key.
+            privateViewKey (PrivateKey): The private view key.
+        """
+
         self.privateSpendKey = privateSpendKey
         self.privateViewKey = privateViewKey
         bs, dummy = secret_expand(privateSpendKey.getPrivateBytes()) # ed25519
@@ -101,6 +249,16 @@ class StealthAddressSigner:
         self.vs = vs
 
     def sign(self, stealthAddress, msg):
+        """Sign the given Stealth Address and message.
+
+        Args:
+            stealthAddress (StealthAddress): The Stealth Address to be signed.
+            msg (bytes): The message to be signed.
+
+        Returns:
+            str: The hexadecimal representation of the signature.
+        """
+
         R, Ps =stealthAddress.R, stealthAddress.Ps
         f = point_compress(point_mul(self.vs, R)) # ed25519
         h = sha512_modq(f)  # ed25519
