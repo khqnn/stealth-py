1315fa60926a097e04e540fb1e1315428f28529c
Khaqan Ashraf <khaqan_ashraf@yahoo.com>
2023-11-24 18:57:53 +0500

    1.0.9 with custom hash function

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='stealth25519',
-    version='1.0.8',
+    version='1.0.9',
     description='The Stealth25519 Python package provides functionality for the generation, verification, and signing of stealth addresses using the standard ed25519 algorithm with the ECDH protocol.',
     long_description=long_description,
     long_description_content_type="text/markdown",
